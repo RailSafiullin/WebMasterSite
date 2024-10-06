ef5c0fc2bb5e571c613f4707bc22dbcff38ae0ef
RailSafiullin <rail.safiyllin@icloud.com>
2024-10-06 13:41:40 +0400

    Delete again

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .comma_newline_speparated_values import CommaNewLineSeparatedValues # noqa
-from .excel import import_users_from_excel, import_users_from_excel_without_password # noqa
+from .excel import import_users_from_excel # noqa
