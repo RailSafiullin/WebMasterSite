8db79fafcb29a0547f6dd49f8c349859a27fd92d
RailSafiullin <rail.safiyllin@icloud.com>
2024-10-06 13:00:42 +0400

    Revert to "Change column username on unique False"

--- utils/excel.py
+++ utils/excel.py
@@ -2,9 +2,9 @@ from typing import Generator
 
 from openpyxl import load_workbook
 
-from api.auth.schemas import UserCreate, UserCreateWithRole, UserCreateWithRoleWithoutPassword
+from api.auth.schemas import UserCreate, UserCreateWithRole
 
-def import_users_from_excel_without_password(file) -> Generator[UserCreateWithRoleWithoutPassword, None, None]:
+def import_users_from_excel(file) -> Generator[UserCreate, None, None]:
     """
     Generator, which converts excel row to UserCreate schema
     :param file: is FileObject
@@ -12,14 +12,4 @@ def import_users_from_excel_without_password(file) -> Generator[UserCreateWithRo
     wb = load_workbook(file, read_only=True, data_only=True)
     for row in wb.active.iter_rows(values_only=True):
         email, username, password, role = map(lambda value: value if value is None else str(value), row)
-        yield UserCreateWithRoleWithoutPassword(id=-1, email=email, username=username, password=password, role=role)
-
-def import_users_from_excel(file) -> Generator[UserCreateWithRole, None, None]:
-    """
-    Generator, which converts excel row to UserCreate schema
-    :param file: is FileObject
-    """
-    wb = load_workbook(file, read_only=True, data_only=True)
-    for row in wb.active.iter_rows(values_only=True):
-        email, username, password, role = map(lambda value: value if value is None else str(value), row)
-        yield UserCreateWithRole(id=-1, email=email, username=username, password="None", role=role)
+        yield UserCreateWithRole(id=-1, email=email, username=username, password=password, role=role)
